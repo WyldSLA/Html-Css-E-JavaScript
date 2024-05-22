83de5475c2a9142cbb2ba4b3cb53ef83bc15b374
Wyldson Marllon <wyldsonmarllon39@gmail.com>
2024-05-22 15:33:13 -0300

    ajustes

--- Exe6 - IMC/Exe6_imc.js
+++ Exe6 - IMC/Exe6_imc.js
@@ -1,11 +1,23 @@
+let peso = document.getElementById("peso_input")
+let altura = document.getElementById("altura_input")
+let msg = document.getElementById("msg_imc")
+
+function analisarValor(peso, altura){
+    if(peso <= 0 || altura <= 0){
+        return true
+    }else{
+        return false
+    }
+}
 
 function calcular(){
-    let peso = document.getElementById("peso_input").value
-    let altura = document.getElementById("altura_input").value
-    let msg = document.getElementById("msg_imc")
-    let ps = parseFloat(peso)
-    let alt = parseFloat(altura) / 100
-    let imc = ps / (alt*alt)
-    msg.innerHTML = `<p>Seu IMC é de: ${imc.toFixed(2)}</p>`
+    if(!analisarValor(peso.value, altura.value)){
+        let ps = parseFloat(peso.value)
+        let alt = parseFloat(altura.value) / 100
+        let imc = ps / (alt*alt)
+        msg.innerHTML = `<p>Seu IMC é de: ${imc.toFixed(2)}</p>`
+    }else{
+        alert("Insira os dados corretamente.")
+    }
 }
 
